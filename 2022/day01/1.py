997dac11a568ad8c2ac36a3a5204f23ff88a2f82
Mudkip <me@mudkip.pw>
2022-12-01 10:24:27 -0330

    Functionize

--- 2022/day01/1.py
+++ 2022/day01/1.py
@@ -1,13 +1,16 @@
-input = open("1.in", "r")
-sums = []
-currSum = 0
-for line in input.readlines():
-    if line == "\n":
-        sums.append(currSum)
-        currSum = 0
-    else:
-        num = int(line.strip())
-        currSum += num
-sums.sort()
-print(f"Part 1: {sums[-1]}")
-print(f"Part 1: {sum(sums[-3:])}")
+def solution(n):
+    input = open("1.in", "r")
+    sums = []
+    currSum = 0
+    for line in input.readlines():
+        if line == "\n":
+            sums.append(currSum)
+            currSum = 0
+        else:
+            num = int(line.strip())
+            currSum += num
+    sums.sort(reverse=True)
+    return sum(sums[:n])
+
+print(f"Part 1: {solution(1)}")
+print(f"Part 2: {solution(3)}")
