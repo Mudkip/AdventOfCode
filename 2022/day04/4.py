dbc2c0bba26fd7a0c60e1c5a028de1852e87b98a
Mudkip <me@mudkip.pw>
2022-12-04 09:23:17 -0330

    Deduplicate

--- 2022/day04/4.py
+++ 2022/day04/4.py
@@ -1,14 +1,10 @@
-def part1():
+def parse_inputs():
     inputs = open('4.in', 'r').read().splitlines()
-    pairs = [(list(map(int, pair[0].split("-"))), list(map(int, pair[1].split("-")))) for pair in [input.split(",") for input in inputs]]
-    engulf = [pair for pair in pairs if (pair[0][0] >= pair[1][0] and pair[0][1] <= pair[1][1]) or (pair[0][0] <= pair[1][0] and pair[0][1] >= pair[1][1])]
-    return len(engulf)
+    return [(list(map(int, pair[0].split("-"))), list(map(int, pair[1].split("-")))) for pair in [input.split(",") for input in inputs]]
 
-def part2():
-    inputs = open('4.in', 'r').read().splitlines()
-    pairs = [(list(map(int, pair[0].split("-"))), list(map(int, pair[1].split("-")))) for pair in [input.split(",") for input in inputs]]
-    engulf = [pair for pair in pairs if max(pair[0][0], pair[1][0]) <= min(pair[0][1], pair[1][1])]
-    return len(engulf)
+def solve(pairs, condition):
+    return len([pair for pair in pairs if condition(pair)])
 
-print(f"Part 1: {part1()}")
-print(f"Part 2: {part2()}")
+pairs = parse_inputs()
+print(f"Part 1: {solve(pairs, lambda pair: (pair[0][0] >= pair[1][0] and pair[0][1] <= pair[1][1]) or (pair[0][0] <= pair[1][0] and pair[0][1] >= pair[1][1]))}")
+print(f"Part 2: {solve(pairs, lambda pair: max(pair[0][0], pair[1][0]) <= min(pair[0][1], pair[1][1]))}")
