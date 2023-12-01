cd25f51d8d9aaf3b47c37eeac4ca4573dc1e05cf
Genadi V <me@mudkip.pw>
2023-12-01 10:20:34 -0330

    Format

--- 2023/day01/1.py
+++ 2023/day01/1.py
@@ -1,13 +1,7 @@
 num_map = {
-    "one": "1",
-    "two": "2",
-    "three": "3",
-    "four": "4",
-    "five": "5",
-    "six": "6",
-    "seven": "7",
-    "eight": "8",
-    "nine": "9"
+    "one": "1", "two": "2", "three": "3",
+    "four": "4", "five": "5", "six": "6",
+    "seven": "7", "eight": "8", "nine": "9"
 }
 
 def parse_input(input):
